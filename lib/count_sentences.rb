32d94e40579229566d4f24da1fccc6063df8dd7c
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-18 23:26:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,6 +29,6 @@ class String
   def count_sentences
     delimiters = [". ", "?", "!"]
     binding.pry
-    self.split(Regexp.union(delimiters))
+    new_count = self.split(Regexp.union(delimiters))
   end
 end
